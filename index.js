c884e5434ad5d4242ad96f5cf99b250455187ce4
ComputersMania <vincenzp3393@gmail.com>
2018-03-24 15:02:45 +0100

    testing

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log('ciao')
-console.warn('attenzione')
-console.error('fuck you')
+const Telegraf = require('telegraf')
+
+let bot = new Telegraf(process.env.BOT_TOKEN)
+
+//bot.telegram.setWebHook(process.env.URI + ':' + process.env.WEBHOOK)
+
+//bot.telegram.startWebHook(process.env.WEBHOOK, process.env.PORT)
+
+
+console.log(process.env.PORT)
+console.warn(typeof process.env.PORT)
