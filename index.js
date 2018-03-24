e7aff4958314cd8837acb337853c9219a55be044
ComputersMania <vincenzp3393@gmail.com>
2018-03-25 00:10:00 +0100

    Openshift nonsense

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Telegraf = require('telegraf')
 
 let bot = new Telegraf(process.env.BOT_TOKEN)
 
-console.log('The webhook is at ' + process.env.URI + ':' + process.env.PORT + process.env.WEBHOOK)
+console.log('The webhook is at ' + process.env.URI + '/' + process.env.WEBHOOK)
 
-bot.telegram.setWebhook(process.env.URI + ':' + process.env.PORT + process.env.WEBHOOK)
+bot.telegram.setWebhook(process.env.URI + '/' + process.env.WEBHOOK)
 
 require('http')
   .createServer( bot.webhookCallback(process.env.WEBHOOK) )
